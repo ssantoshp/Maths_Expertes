48914f56ab145f5892c12b966d12cbbf8daaa564
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-09-19 20:57:09 +0200

    Create prime_number_decomposer.py

---
+++ prime_number_decomposer.py
@@ -0,0 +1,17 @@
+n = eval(input())
+
+print("\n")
+
+i = 2
+factors = []
+
+while i * i <= n:
+  if n % i:
+    i += 1
+  else:
+    n //= i
+    factors.append(i)
+if n > 1:
+  factors.append(n)
+
+print(factors)
