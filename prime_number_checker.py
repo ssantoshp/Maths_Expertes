d22186893b7d58699a5db7470574e72d7a179e8d
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-09-28 11:26:03 +0200

    Update prime_number_checker.py

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,15 +1,16 @@
-num = eval(input())
-print("\n")
-if num > 1:
- 
-    for i in range(2, int(num/2)+1):
+while True:
+  num = eval(input())
+  print("\n")
+  if num > 1:
+  
+      for i in range(2, int(num/2)+1):
 
-        if (num % i) == 0:
-            print(i)
-            print(num, "est pas un nombre premier")
-            break
-    else:
-        print(num, "est un nombre premier")
- 
-else:
-    print(num, "est pas un nombre premier")
+          if (num % i) == 0:
+              print(i)
+              print(num, "est pas un nombre premier")
+              break
+      else:
+          print(num, "est un nombre premier")
+  
+  else:
+      print(num, "est pas un nombre premier")
