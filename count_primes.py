9ebfde64443ea04a7acac9c95a51cd12bd357e25
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-09-28 11:54:56 +0200

    Update count_primes.py

--- count_primes.py
+++ count_primes.py
@@ -4,4 +4,6 @@ while True:
     for i in range(3,int(n**0.5)+1,2):
         if sieve[i//2]:
             sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
-    print([2] + [2*i+1 for i in range(1,n//2) if sieve[i]])
+    final=[2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
+    print(final)
+    print("Il y en a: "+ str(len(final)))
