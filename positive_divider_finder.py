dd7351b16cbbb6f7cdf757babb5c7e7bb0ef4951
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-09-28 11:24:50 +0200

    Update positive_divider_finder.py

--- positive_divider_finder.py
+++ positive_divider_finder.py
@@ -1,14 +1,16 @@
 import math
 
-n=eval(input())
-print("\n")
-nsqrt=math.sqrt(n)
-new_n=math.floor(nsqrt)
 
-for i in range(1,new_n+1):
-  if n%i==0:
-    print(i)
-    if i==int(n/i):
-      pass
-    else:
-      print(int(n/i))
+while True:
+  n=eval(input())
+  print("\n")
+  nsqrt=math.sqrt(n)
+  new_n=math.floor(nsqrt)
+
+  for i in range(1,new_n+1):
+    if n%i==0:
+      print(i)
+      if i==int(n/i):
+        pass
+      else:
+        print(int(n/i))
