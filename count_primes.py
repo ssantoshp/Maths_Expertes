b5c538b1dfdfd88a1bf70b73c0b6fa1d4eee43f2
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-09-28 11:52:43 +0200

    Create count_primes.py

---
+++ count_primes.py
@@ -0,0 +1,7 @@
+while True:
+    n = eval(input())
+    sieve = [True] * (n//2)
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i//2]:
+            sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
+    print([2] + [2*i+1 for i in range(1,n//2) if sieve[i]])
